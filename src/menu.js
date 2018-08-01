d2af12d4ce20012d632fa3d015f413d89ac80d4a
Khoi Le <minarek@outlook.com>
2018-08-01 16:56:39 +0200

    Fixed bugs

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => (
-    <ul class='main-menu'>
+    <ul className='main-menu'>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/products'>Products</Link></li>
         <li><Link to='/about'>About</Link></li>
